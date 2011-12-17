2e033d1099c97190738bff63010103c602a31c92
John Skrzypek <jms8142@hotmail.com>
2011-12-16 22:43:29 -0500

    Initial Commit

---
+++ miner/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  miner
+//
+//  Created by John Skrzypek on 12/16/11.
+//  Copyright 2011 One Eleven Interactive. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
